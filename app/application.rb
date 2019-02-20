ed953ed63cbc920ff7c97c871d48a15b5d6bef1b
Mitchel Parish <mitchelparish@gmail.com>
2019-02-20 21:06:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
 
       item_name = req.path.split("/items").last
-      item = @@item.find{|i| i.title == item_name}
+      item = @@item.find{|i| i.name == item_name}
 
       resp.write item
 
